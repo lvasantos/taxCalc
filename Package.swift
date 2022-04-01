a73909d41b0ed2bc6dea22797ba3842aacd159b1
Luciana Adriao <luicianaasantos@hotmail.com>
2022-04-01 10:46:43 -0300

    execApp

--- Package.swift
+++ Package.swift
@@ -5,22 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "conversor",
-    products:  [
-        .library(name: "conversor", targets: ["conversor"]),
-    ],
+    products: [
+            .library(name: "conversor", targets:["conversor"]),
+            .executable(name: "conversorExec", targets: ["conversorExec"])
+        ],
     dependencies: [
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "1.7.4"),
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "conversor",
-            dependencies: ["SwiftSoup"]),
-        .testTarget(
-            name: "conversorTests",
-            dependencies: ["conversor"]),
-    ]
+            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+            // Targets can depend on other targets in this package, and on products in packages this package depends on.
+            .target(
+                name: "conversor",
+                dependencies: ["SwiftSoup"]),
+            .executableTarget(
+                name: "conversorExec",
+                dependencies: ["conversor"]
+            ),
+            .testTarget(
+                name: "conversorTests",
+                dependencies: ["conversor"]),
+        ]
 )
---
+++ Sources/conversorExec/main.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by Luciana Adrião on 01/04/22.
+//
+
+import Foundation
+import conversor
+
+App().main()
